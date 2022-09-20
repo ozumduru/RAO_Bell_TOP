ab85e004302c8d1c64967feac679d90ecd382fc9
Özüm Duru <41477253+ozumduru@users.noreply.github.com>
2022-09-20 21:08:34 +0300

    Update throat_inlet.m

--- throat_inlet.m
+++ throat_inlet.m
@@ -1,16 +1,13 @@
-function [x,y,throat_i_l,throat_i_h]=throat_inlet(Rt,theta_t_i)
+function [x,y]=throat_inlet(Rt,theta_t_i,step_count)
 
-theta_t_i = theta_t_i * pi /180;
+throat_i_l = 1.5*Rt*sind(theta_t_i);
 
-throat_i_l = 1.5*Rt*sin(theta_t_i);
-throat_i_h = 2.5*Rt - 1.5*Rt*cos(theta_t_i);
-
-angles = linspace(3*pi/2 - theta_t_i ,3*pi/2,50);
+angles = linspace(270 - theta_t_i, 270, step_count);
 
 Xc = throat_i_l;
 Yc = 2.5*Rt;
 
-x = 1.5*Rt*cos(angles) + Xc ;
-y = 1.5*Rt*sin(angles) + Yc ;
+x = 1.5*Rt*cosd(angles) + Xc ;
+y = 1.5*Rt*sind(angles) + Yc ;
 
 end
