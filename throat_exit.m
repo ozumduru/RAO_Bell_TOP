c594c72b1960b13381597c238415ad48d1c20027
Özüm Duru <41477253+ozumduru@users.noreply.github.com>
2022-09-20 21:09:03 +0300

    Update throat_exit.m

--- throat_exit.m
+++ throat_exit.m
@@ -1,16 +1,13 @@
-function[x,y,throat_e_l,throat_e_h]=throat_exit(Rt,theta_N)
+function[x,y]=throat_exit(Rt,theta_N,step_count)
 
-theta_N = theta_N * pi / 180;
+throat_e_l = 0.382*Rt*sind(theta_N);
 
-throat_e_l = 0.382*Rt*sin(theta_N);
-throat_e_h = 1.382*Rt - 0.382*Rt*cos(theta_N);
-
-angles = linspace(3*pi/2 , 3*pi/2 + theta_N ,50);
+angles = linspace(270 , 270 + theta_N ,step_count);
 
 Xc = throat_e_l;
 Yc = 1.382*Rt;
 
-x = 0.382*Rt*cos(angles) + Xc ;
-y = 0.382*Rt*sin(angles) + Yc ;
+x = 0.382*Rt*cosd(angles) + Xc ;
+y = 0.382*Rt*sind(angles) + Yc ;
 
 end
